fe7b14920953228ee340a1b95dbd83007bc9f185
Alexander S <alexander@sharapov.biz>
2024-10-26 18:07:38 +0600

    Init project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,16 @@
             </div>
         @endif
 
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
         <h1>Мои бронирования</h1>
 
         @if($bookings->isEmpty())
